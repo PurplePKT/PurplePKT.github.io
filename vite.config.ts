716b72339822810fbaa41315c2e18818f632495b
PurplePKT <212429700+PurplePKT@users.noreply.github.com>
2025-05-23 02:34:37 +0000

    Build for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: '/', // For https://purplepkt.github.io/ use '/'
   plugins: [
     react(),
     runtimeErrorOverlay(),
