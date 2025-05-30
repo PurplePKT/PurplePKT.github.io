c9fe717b6536dc490c212ec6519a92b2b846cdf8
PurplePKT <212429700+PurplePKT@users.noreply.github.com>
2025-05-30 03:12:24 +0000

    Revert back to working Preview

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,52 +1,32 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Route, Switch } from "wouter";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
-import Navbar from "@/components/layout/navbar";
-import Footer from "@/components/layout/footer";
-import Home from "@/pages/home";
-import Routes from "@/pages/routes";
-import RouteDetails from "@/pages/route-details";
-import Solicitations from "@/pages/solicitations";
-import About from "@/pages/about";
-import Contact from "@/pages/contact";
-import Login from "@/pages/login";
-import Apply from "@/pages/apply";
-import Admin from "@/pages/admin";
-
-function Router() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow">
-        <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/routes" component={Routes} />
-          <Route path="/routes/:id" component={RouteDetails} />
-          <Route path="/solicitations" component={Solicitations} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/login" component={Login} />
-          <Route path="/apply/:id" component={Apply} />
-          <Route path="/admin" component={Admin} />
-          <Route component={NotFound} />
-        </Switch>
-      </main>
-      <Footer />
-    </div>
-  );
-}
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Routes from "./pages/Routes";
+import Solicitations from "./pages/Solicitations";
+import About from "./pages/About";
+import CsvDocumentation from "./pages/CsvDocumentation";
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/routes" component={Routes} />
+            <Route path="/solicitations" component={Solicitations} />
+            <Route path="/about" component={About} />
+            <Route path="/documentation" component={CsvDocumentation} />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
+        <Footer />
+      </div>
+    </TooltipProvider>
   );
 }
 
